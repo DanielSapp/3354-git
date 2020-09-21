15c2ae8c3f6e7f69500a1a417c2169e7d643976e
Eni Sapp <drs190000@utdallas.edu>
2020-09-21 04:44:35 -0500

    Fixed bug in main() exception throwing
    Changed a non-existent exception being thrown into a valid one.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,16 +1,19 @@
 package adder;
 
+import java.util.InputMismatchException;
+import java.lang.IllegalArgumentException;
+
 public class Main {
 
     public static void main(String[] args) {
         try {
             int result = addArguments(args);
             System.out.println(result);
-        } catch (InvalidInputException  e) {
+        } catch (IllegalArgumentException  e) {
             System.err.println("Please provide 1 or more arguments.  Usage: ./Main [-] numOne [numTwo [...]]");
         } catch (InputMismatchException e) {
 	    System.err.println("Invalid input.  Only pass in integers or a minus as the first argument.  Usage: ./Main [-] numOne [numTwo [...]]");
-	}
+	    }
     }
 
     private static int addArguments(String[] args) {
